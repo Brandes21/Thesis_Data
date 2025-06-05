60b8b378e58b9e83d0b191423f42fea6deef6d7f
Niclas Brandt <141432657+Brandes21@users.noreply.github.com>
2025-06-05 17:15:01 +0200

    Update bell_curve.py

--- Ribbed_floor/bell_curve.py
+++ Ribbed_floor/bell_curve.py
@@ -35,27 +35,3 @@ plt.grid(True)
 plt.show()
 
 
-# import numpy as np 
-# import matplotlib.pyplot as plt 
-  
-# # A custom function to calculate 
-# # probability distribution function 
-# def pdf(x): 
-#     mean = np.mean(x) 
-#     std = np.std(x) 
-#     y_out = 1/(std * np.sqrt(2 * np.pi)) * np.exp( - (x - mean)**2 / (2 * std**2)) 
-#     return y_out 
-    
-
-# x = df
-  
-# y = pdf(x) 
-  
-# # Plotting 
-# plt.style.use('seaborn') 
-# plt.figure(figsize = (6, 6)) 
-# plt.plot(x, y, color = 'black', 
-#          linestyle = 'dashed') 
-  
-# plt.scatter( x, y, marker = 'o', s = 25, color = 'red') 
-# plt.show()
